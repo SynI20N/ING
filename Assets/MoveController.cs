6ff80a644a637348b5c4e7d6bc906e8b42a635b9
SynI20N <69692013+SynI20N@users.noreply.github.com>
2024-03-14 19:39:45 +0300

    Add files via upload

---
+++ Assets/MoveController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f; //1 / ms
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 direction = new Vector3(0,0,0);
+        if(Input.GetKey("d"))
+        {
+            direction = new Vector3(1, 0, 0);
+        }
+        if(Input.GetKey("a"))
+        {
+            direction = new Vector3(-1, 0, 0);
+        }
+        if(Input.GetKey("w"))
+        {
+            direction = new Vector3(0, 1, 0);
+        }
+        if(Input.GetKey("s"))
+        {
+            direction = new Vector3(0, -1, 0);
+        }
+        if(Input.anyKey)
+        {
+            Vector3 moveVector = _speed * Time.deltaTime * direction;
+            transform.position += moveVector;
+        }
+    }
+}
