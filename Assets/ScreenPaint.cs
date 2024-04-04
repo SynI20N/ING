0677147f04ce09238d4783903a7e49261167b52f
SynI20N <69692013+SynI20N@users.noreply.github.com>
2024-04-04 19:04:22 +0300

    Add files via upload

--- Assets/ScreenPaint.cs
+++ Assets/ScreenPaint.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class ScreenPaint : MonoBehaviour
 {
-    [SerializeField] private Camera cam;
-    [SerializeField] private GameObject circle;
-    [SerializeField] private int pixelPathToDraw;
-    private Vector3 startPos;
-    private Vector3 currentPos;
+    [SerializeField] private Camera _cam;
+    [SerializeField] private GameObject _circle;
+    [SerializeField] private float _distanceToDraw;
+    private Vector3 _startPos;
+    private Vector3 _currentPos;
+    private List<GameObject> _spawnedCircles = new List<GameObject>();
 
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            startPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            _startPos = _cam.ScreenToWorldPoint(Input.mousePosition);
         }
         if(Input.GetMouseButton(0))
         {
-            currentPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            _currentPos = _cam.ScreenToWorldPoint(Input.mousePosition);
+            //Debug.Log($"Mouse position is {Input.mousePosition}, _currentPos is {_currentPos}");
         }
-        if((currentPos - startPos).magnitude > pixelPathToDraw)
+        if((_currentPos - _startPos).magnitude > _distanceToDraw)
         {
-            GameObject obj = Instantiate(circle);
-            Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
-            obj.transform.position = new Vector3(pos.x, pos.y, -5f);
+            Vector3 direction = _currentPos - _startPos;
+            int step = 30;
+            for(int i = 0; i < step; i++)
+            {
+                Vector3 dir = _startPos + direction * i / step;
+                GameObject obj = Instantiate(_circle);
+                _spawnedCircles.Add(obj);
+                obj.transform.position = new Vector3(dir.x, dir.y, -5f);
+            }
+            _startPos = _currentPos;//я забыл эту строчку
         }
+    }
+
+    public void ClearScreen()
+    {
+        foreach(GameObject circle in _spawnedCircles)
+        {
+            Destroy(circle);
+        }
+        _spawnedCircles.Clear();
     }
 }
 
@@ -34,3 +52,17 @@ public class ScreenPaint : MonoBehaviour
 на координатах мыши
 с частотой 1 круг на 5 пикселей
 */
+
+// получились лучи на экране. можно исследовать почему?
+// if((_currentPos - _startPos).magnitude > _distanceToDraw)
+// {
+//     Vector3 direction = _currentPos - _startPos;
+//     int step = 100;
+//     for(int i = 0; i < step; i++)
+//     {
+//         direction = _startPos + direction * i / step;
+//         GameObject obj = Instantiate(_circle);
+//         obj.transform.position = new Vector3(direction.x, direction.y, -5f);
+//     }
+//     _startPos = _currentPos;//я забыл эту строчку
+// }
