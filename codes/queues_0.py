5a4c5498ae8f35bb4f14f6e9a53fc5a4f20c07c2
SynI20N <69692013+SynI20N@users.noreply.github.com>
2024-04-07 15:20:54 +0300

    Add files via upload

--- codes/queues_0.py
+++ codes/queues_0.py
@@ -1,15 +1,12 @@
 import pymurapi as mur
 import time
-import matplotlib.pyplot as plt
+import queue
 import threading as th
 
 auv = mur.mur_init()
 prev_time = 0
-prev_time_while = 0
-prev_depth = 0.0
 prev_error = 0.0
-times = []
-velocities = []
+
 def clamp(v, max_v, min_v):
     if v > max_v:
         return max_v
@@ -24,38 +21,30 @@ def keep_depth(value):
     error = auv.get_depth() - value
     power_2 = 0
     power_3 = 0
-    power_value = error * 70
-    diff_value = 2 / (current_time - prev_time) * (error - prev_error)
+    power_value = error * 200
+    diff_value = 5 / (current_time - prev_time) * (error - prev_error)
     power_2 = clamp(power_value + diff_value, 100, -100)
     power_3 = clamp(power_value + diff_value, 100, -100)
-    auv.set_motor_power(2, power_2)
+    auv.set_motor_power(0, power_2)
     auv.set_motor_power(3, power_3)
     prev_time = current_time
     prev_error = error
-   
     
-start = time.time()
-current = start
-while current - start < 3:
-    current = time.time()
-    print(current - start)
-    keep_depth(2)
-    time.sleep(0.03)
-start = time.time()
-current = start
-velocity = 0
-while current - start < 5 or abs(auv.get_depth() - 1) > 0.01 or velocity > 1:
-    current_time = int(round(time.time() * 1000))
-    current_depth = auv.get_depth()
-    current = time.time()
-    print(str(current - start) + " " + str(velocity))
-    velocity = (current_time - prev_time_while) * (current_depth - prev_depth)
-    prev_time_while = current_time
-    prev_depth = current_depth
-    keep_depth(1)
-    time.sleep(0.03)
-    times.append(time.time())
-    velocities.append(velocity)
-    plt.plot(times, velocities)
-    plt.ylim(-200, 200)
-    plt.pause(0.5)
+def keep_depth_t (timeD, depth):
+    prev_time_0 = time.time()
+    while abs(time.time() - prev_time_0) < timeD:
+        keep_depth(depth)
+        print(depth)
+        time.sleep(0.03)
+        
+
+commands = queue.Queue()
+commands.put(lambda: keep_depth_t(5, -0.4))
+commands.put(lambda: keep_depth_t(5, -0.2))
+commands.put(lambda: keep_depth_t(5, 0))
+times = [3, 4, 2]
+
+n = 3
+for i in range(n):
+    cmd = commands.get(timeout=times[i])
+    cmd()
